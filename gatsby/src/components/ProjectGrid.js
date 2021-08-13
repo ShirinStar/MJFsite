f878b933fa475d067c3284a5ae5d3a9b6ec1c237
shirinstar <shirinanlen@gmail.com>
2021-08-12 17:04:13 -0700

    layout grid and project page

---
+++ gatsby/src/components/ProjectGrid.js
@@ -0,0 +1,37 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+// const ProjectStyle = styled.div`
+/* display: grid;
+  @supports not (grid-template-rows: subgrid) {
+    grid-template-rows: auto auto 1fr;
+  }
+  grid-template-rows: subgrid;
+  column-gap: 20px;
+  row-gap: 10px;
+  h2,
+  p {
+    margin: 0;
+  } */
+// `;
+
+export default function ProjectPage({ project }) {
+  return (
+    <>
+      <div className="imageContainer">
+        <Link to={`/project/${project.slug.current}`}>
+          <div className="imageGrid">
+            <Img fixed={project.image.asset.fixed} alt={project.name} />
+          </div>
+          <div className="textImages textImages--blur">
+            <p className="mainImageTitle">{project.name}</p>
+            <p className="secondImageTitle">{project.tagline}</p>
+            {/* <p>{project.tags.map((tag) => tag.name).join(', ')}</p> */}
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+}
--- gatsby/src/components/ProjectsList.js
+++ gatsby/src/components/ProjectsList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import ProjectPage from './ProjectPage';
+import ProjectPage from './ProjectGrid';
 
 // const ProjectGridStyle = styled.div`
 /* display: grid;
